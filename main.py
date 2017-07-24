c2d0c8d3eaadbc413c595a9bde105806270161bc
Jared Haer <jaredhaer@gmail.com>
2017-07-24 14:17:45 -0500

    Using different method for handling cache

--- main.py
+++ main.py
@@ -41,13 +41,15 @@ def bandName():
     return render_template("band_name.html", page_info=page_info)
 
 @app.after_request
-def add_header(response):
+def add_header(r):
     """
     Add headers to both force latest IE rendering engine or Chrome Frame,
     and also to cache the rendered page for 10 minutes.
     """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5004)
